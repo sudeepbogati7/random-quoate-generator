2878695d0e41758ffea4b5f504d0131b59cade4e
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-02-23 13:33:45 +0545

    minor changes on quotes part

--- js/quotes.js
+++ js/quotes.js
@@ -1,17 +1,21 @@
-function getQuotes(){
-    fetch('https://api.quotable.dev/quotes')
-        .then((res) => {
-            if(!res.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return res.json();
-        })
-        .then((result) => {
-            console.log(result);
-        })
-        .catch(err => {
-            console.error("Error fetching quotes", err);
-        });
+async function getQuotes() {
+    const url = 'https://juanroldan1989-moviequotes-v1.p.rapidapi.com/api/v1/quotes?actor=Al%20Pacino';
+    const options = {
+        method: 'GET',
+        headers: {
+            Authorization: 'Token token=yd8WzkWNEEzGtqMSgiZBrwtt',
+            'X-RapidAPI-Key': '788321e4bemsh539392ce40a9a84p1b73b7jsn448d3d49cf0b',
+            'X-RapidAPI-Host': 'juanroldan1989-moviequotes-v1.p.rapidapi.com'
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 getQuotes();
